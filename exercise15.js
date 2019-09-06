58668fb4163f7ce72568c8c116d3481f0a64fd5e
Muhammad Iqbal <iqbal.itang@gmail.com>
2019-09-06 16:17:19 +0700

    add new exercise

---
+++ exercise15.js
@@ -0,0 +1,24 @@
+
+function groupAnimals(animals) {
+    // you can only write your code here!
+    var animal = animals.sort()
+    var output = []
+    for(var i = 0; i < animal.length; i++) {
+      var hurufAwal = animal[i][0]
+      var urutan = [animal[i]]
+      for(var j = animal.length -1; j > i; j--) {
+        if(hurufAwal === animal[j][0]) {
+          urutan.push(animal[j])
+          animal.splice(j, 1)
+        }
+      }
+    output.push(urutan)
+    }
+    return output;
+  }
+  
+  // TEST CASES
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+  // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] 
---
+++ exercise2.js
@@ -0,0 +1,8 @@
+function balikString (str) {
+    var input = "";
+    for(var i = str.length -1; i >= 0; i--){
+        input += str[i]
+    }
+    return input;
+}
+console.log(balikString("hello world"))
